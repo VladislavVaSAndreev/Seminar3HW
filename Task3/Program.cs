100f7ae1fffaa66f46f51d0fc193d4c0d848a26f
VladislavAndreev <andreev.92@inbox.ru>
2023-06-21 20:49:48 +0300

    addTask3

---
+++ Task3/Program.cs
@@ -0,0 +1,16 @@
+﻿int ReadInt(string message)
+{
+    Console.Write($"{message} > ");
+    int value = int.Parse(Console.ReadLine());
+    return value;
+}
+
+int number = ReadInt("Введите целое положительное число>");
+
+int x = 1;
+while (x <= number)
+{
+    int result = x * x * x;
+    Console.Write($" {result} ");
+    x++;
+}

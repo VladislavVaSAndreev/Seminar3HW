10fa93e2a06e9a2a4eb372233b2db8bca99d456c
VladislavAndreev <andreev.92@inbox.ru>
2023-06-21 20:45:23 +0300

    addTask1

---
+++ Task1/Program.cs
@@ -0,0 +1,28 @@
+﻿int ReadInt(string message)
+{
+    Console.Write($"{message} > ");
+    int value = int.Parse(Console.ReadLine());
+    return value;
+}
+
+bool Validate5sign(int number)
+{
+    if (number > 9999 && number <= 99999)
+    {
+        return true;
+    }
+    System.Console.WriteLine("Это число не пятизначное");
+    return false;
+}
+int number = ReadInt("Введите число >");
+if (Validate5sign(number))
+{
+    int firstDigit = number/10000;
+    int secondDigit =number/1000%10;
+    int fourthDigit = number%100/10;
+    int fifthDigit = number%10;
+if (firstDigit==fifthDigit && secondDigit==fourthDigit)    
+System.Console.WriteLine($"Это полиндром");
+else 
+{System.Console.WriteLine($"Это не полиндром");}
+}
